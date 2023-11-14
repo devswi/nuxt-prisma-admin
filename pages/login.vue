369f7086e8319382a72cf84b4d0fd81f9279d0d8
shiwei93 <stayfocusjs@gmail.com>
2023-11-14 19:48:54 +0800

    fix: incorrect logo in dark mode

--- pages/login.vue
+++ pages/login.vue
@@ -3,7 +3,6 @@ definePageMeta({
   layout: 'blank',
 })
 
-const colorMode = useColorMode()
 const currentUser = useAuthUser()
 const isAdmin = useAdmin()
 const { login } = useAuth()
@@ -21,7 +20,6 @@ const passwordVisible = ref(false)
 
 const inputType = computed(() => (passwordVisible.value ? 'text' : 'password'))
 const eyeIcon = computed(() => passwordVisible.value ? 'i-heroicons-eye-slash-solid' : 'i-heroicons-eye-solid')
-const logo = computed(() => colorMode.value === 'dark' ? '/logo-dark.png' : '/logo.png')
 
 const handleLoginSuccess = async () => {
   const redirect = isAdmin.value ? '/admin' : '/'
@@ -43,7 +41,12 @@ const handleLogin = async () => {
 
 <template>
   <div class="h-screen flex flex-col items-center justify-center">
-    <NuxtImg class="mb-24" :src="logo" />
+    <div class="mb-24">
+      <svg xmlns="http://www.w3.org/2000/svg" width="98" height="98" viewBox="0 0 98 98" fill="none">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M78.2819 19.3018L51.5571 5.93939V36.1129L19.303 52.2399V78.2806L46.0278 91.643V61.4695L78.2819 45.3425V19.3018ZM78.2819 52.2399V78.2806L51.5571 91.643V65.6023L78.2819 52.2399ZM19.303 19.3018V45.3424L46.0278 31.9797V5.93939L19.303 19.3018Z" fill="rgb(var(--color-primary-DEFAULT))" fill-opacity="0.6" />
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M19.3033 19.3018L46.0281 5.93939V36.1129L78.2822 52.2399V78.2806L51.5574 91.643V61.4695L19.3033 45.3425V19.3018ZM19.3033 52.2399V78.2806L46.0281 91.643V65.6023L19.3033 52.2399ZM78.2822 19.3018V45.3424L51.5574 31.9797V5.93939L78.2822 19.3018Z" fill="rgb(var(--color-primary-DEFAULT))" />
+      </svg>
+    </div>
     <div class="flex flex-col w-80">
       <UInput
         v-model="form.data.username"
